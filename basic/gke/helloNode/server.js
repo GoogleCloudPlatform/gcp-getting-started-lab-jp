7dfdb83b00b338319f7a29b3433386ac798a51c4
kkitase <kkitase@gmail.com>
2019-06-20 17:59:52 +0900

    remove blank

--- basic/gke/helloNode/server.js
+++ basic/gke/helloNode/server.js
@@ -1,11 +1,9 @@
 var http = require('http');
-
 var handleRequest = function(request, response) {
   var os = require('os');
   var hostname = os.hostname();
   response.writeHead(200);
   response.end("<h1>Hello World! == " + hostname + "</h1>\n");
 }
-
 var www = http.createServer(handleRequest);
 www.listen(8080);
