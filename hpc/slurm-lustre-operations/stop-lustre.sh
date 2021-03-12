9fa9976bd845988ebfc1a7cd4da53ccb6bd5e9c1
nosu <nosu@users.noreply.github.com>
2021-03-12 21:42:09 +0900

    Fix a bug
    Remove double quotations for loop variables

--- hpc/slurm-lustre-operations/stop-lustre.sh
+++ hpc/slurm-lustre-operations/stop-lustre.sh
@@ -3,13 +3,13 @@
 oss=$( gcloud deployment-manager deployments describe lustre --format json \
     | jq -r '.resources[]|select(.type=="compute.v1.instance")|.name' \
     | grep oss)
-for id in "${oss}"; do
+for id in ${oss}; do
     gcloud compute instances stop "${id}" --zone=asia-northeast1-c
 done
 
 mds=$( gcloud deployment-manager deployments describe lustre --format json \
     | jq -r '.resources[]|select(.type=="compute.v1.instance")|.name' \
     | grep mds)
-for id in "${mds}"; do
+for id in ${mds}; do
     gcloud compute instances stop "${id}" --zone=asia-northeast1-c
 done
