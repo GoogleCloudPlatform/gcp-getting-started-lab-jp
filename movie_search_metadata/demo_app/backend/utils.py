22950e54b3ca1692711249d2311c26d3c774e1f4
Etsuji Nakai <enakai0@gmail.com>
2024-11-13 10:31:47 +0900

    Update utils.py

--- movie_search_metadata/demo_app/backend/utils.py
+++ movie_search_metadata/demo_app/backend/utils.py
@@ -1,23 +1,9 @@
 import datetime
 from google.cloud import storage
 from google import auth
-from google.auth import impersonated_credentials
 
 
-def get_impersonated_credentials():
-    scopes=['https://www.googleapis.com/auth/cloud-platform']
-    credentials, project = auth.default(scopes=scopes)
-    if credentials.token is None:
-        credentials.refresh(auth.transport.requests.Request())
-    signing_credentials = impersonated_credentials.Credentials(
-        source_credentials=credentials,
-        target_principal=credentials.service_account_email,
-        target_scopes=scopes,
-        lifetime=datetime.timedelta(seconds=3600),
-        delegates=[credentials.service_account_email]
-    )
-    return signing_credentials
-
+credentials, project_id = auth.default()
 
 def generate_download_signed_url_v4(bucket_name: str, blob_name: str) -> str:
     """Cloud Storage の Blob の v4 signed URL を生成する
@@ -29,6 +15,10 @@ def generate_download_signed_url_v4(bucket_name: str, blob_name: str) -> str:
     Returns:
         署名付きURL
     """
+
+    if not credentials.valid:
+        credentials.refresh(auth.transport.requests.Request())
+
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
@@ -37,7 +27,8 @@ def generate_download_signed_url_v4(bucket_name: str, blob_name: str) -> str:
         version="v4",
         expiration=datetime.timedelta(minutes=15),
         method="GET",
-        credentials=get_impersonated_credentials()
+        access_token=credentials.token,
+        service_account_email=credentials.service_account_email
     )
 
     return url
