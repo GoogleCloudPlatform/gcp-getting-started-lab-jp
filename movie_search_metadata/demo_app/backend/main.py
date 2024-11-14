185453dc832a80ae3f5640017520af657ad3a974
Etsuji Nakai <enakai0@gmail.com>
2024-11-14 09:09:17 +0900

    Update main.py

--- movie_search_metadata/demo_app/backend/main.py
+++ movie_search_metadata/demo_app/backend/main.py
@@ -46,4 +46,4 @@ async def api_scene_search(
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host='0.0.0.0', port=port)
