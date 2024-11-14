a38c96ea45e3000091ae60e67d3d12db610757d4
Etsuji Nakai <enakai0@gmail.com>
2024-11-14 10:58:58 +0000

    Update

--- movie_search_metadata/demo_app/vais_setup.sh
+++ movie_search_metadata/demo_app/vais_setup.sh
@@ -3,6 +3,25 @@
 export PROJECT_ID=$(gcloud config list --format "value(core.project)")
 export BUCKET="gs://${PROJECT_ID}-movie-search"
 
+echo ""
+echo "## Enabling APIs..."
+
+services=(
+  "discoveryengine.googleapis.com"
+)
+services_list="(""$(IFS='|'; echo "${services[*]}")"")"
+enabled=$(gcloud services list --format json | jq .[].config.name |\
+  grep -E "$services_list" | wc -l)
+if [[ $enabled != ${#services[@]} ]]; then
+  echo "Enabling APIs."
+  services_list="$(IFS=' '; echo "${services[*]}")"
+  gcloud services enable $services_list
+
+  echo "Wait 60 seconds for APIs to be ready."
+  sleep 60
+fi
+
+echo ""
 echo "## Creating bucket..."
 gsutil mb -b on -l us-central1 $BUCKET
 
