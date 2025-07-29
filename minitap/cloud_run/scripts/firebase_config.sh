43e7308ae38ab5c31937f8a7d81d56f381848038
Tayzar <tayzar.tznw@gmail.com>
2025-07-29 18:10:10 +0900

    Added scripts

---
+++ minitap/cloud_run/scripts/firebase_config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+APP_ID=$(firebase -j apps:list -P $PROJECT_ID | jq '.result[] | select(.displayName == "streamchat")' | jq -r '.appId')
+TMP_FILE=firebaseConfig.json
+ENV_PATH=./src/streamchat
+
+if [ ! -z ${1} ]; then
+    ENV_PATH=$1
+fi
+
+firebase -j apps:sdkconfig -P $PROJECT_ID WEB $APP_ID | jq '.result.sdkConfig' > $TMP_FILE
+
+function update_env() {
+    json_key=$1
+    env_key=$2
+    json_value=$(cat firebaseConfig.json | jq ".$json_key")
+    sed -i -e "s/$env_key=$/$env_key=$json_value/g" $ENV_PATH/.env
+}
+
+update_env projectId NEXT_PUBLIC_FIREBASE_PROJECT_ID
+update_env appId NEXT_PUBLIC_FIREBASE_APP_ID
+update_env storageBucket NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+update_env apiKey NEXT_PUBLIC_FIREBASE_API_KEY
+update_env authDomain NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+update_env messagingSenderId NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+
+echo "Updated configuration..."
+echo 
+cat $ENV_PATH/.env
+
+rm -rf $TMP_FILE
