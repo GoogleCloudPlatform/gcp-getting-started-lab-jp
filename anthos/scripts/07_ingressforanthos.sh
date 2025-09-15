971b8301ac8291ef4262d319dbc1e7c76fecd558
Tayzar <tayzar.tznw@gmail.com>
2025-09-15 22:51:08 +0900

    Env for cloudshell

--- anthos/scripts/07_ingressforanthos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ./script/init.env
-
-gcloud services enable multiclusteringress.googleapis.com
-gcloud alpha container hub ingress enable \
-  --config-membership=projects/${PROJECT_ID}/locations/global/memberships/${CLUSTER2}
