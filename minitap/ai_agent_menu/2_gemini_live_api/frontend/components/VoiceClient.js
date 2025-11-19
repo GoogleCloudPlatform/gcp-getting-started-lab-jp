74780383c913384f8058aed679569c0f2f2df222
Yuting <linyuting@google.com>
2025-11-19 18:56:52 +0900

    Update VoiceClient.js

--- minitap/ai_agent_menu/2_gemini_live_api/frontend/components/VoiceClient.js
+++ minitap/ai_agent_menu/2_gemini_live_api/frontend/components/VoiceClient.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import ReactMarkdown from 'react-markdown';
 import { VoicecallBackendAPI } from "lib/voicecall-backend";
 import {
   LiveAudioInputManager,
